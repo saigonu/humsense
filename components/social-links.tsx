6861d8b40fa26f49614a4b66a1ac81e0348e9e9a
Sai Gonuguntla <75554919+saigonu@users.noreply.github.com>
2024-03-20 09:37:19 -0700

    updated code

---
+++ components/social-links.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaLinks = () => {
+    return (
+        <div className='flex flex-row gap-4'>
+            <a href="https://github.com/saigonu" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/saigonuguntla/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
